9c38135331b167e9d47fdee9e65e9b3700a1f187
Alex Krav <faiger.sys@gmail.com>
2016-05-11 17:35:57 +0300

    Some improve, double 2.....

--- src/FaigerSYS/superBAR/Main.php
+++ src/FaigerSYS/superBAR/Main.php
@@ -30,8 +30,8 @@ class Main extends PluginBase {
 		$this->FORMAT = $this->config->get("format");
 		$this->TIME_FORMAT = $this->config->get("time-format");
 		$this->noF = $this->config->get("no-faction");
-		if ($this->config->get("type") == "popup" || $this->config->get("type") == null)
-			$this->popup = true;
+		if ($this->config->get("type") !== "popup")
+			$this->popup = false;
 		
 		$ticks = preg_replace("/[^0-9]/", '', $this->config->get("timer"));
 		
