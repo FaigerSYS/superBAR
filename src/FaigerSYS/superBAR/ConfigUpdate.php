aaafeeaff8a11fa7b7a3224a086b75bdb630f2cc
Alex Krav <faiger.sys@gmail.com>
2016-05-17 08:11:58 +0300

    Version 1.3.5 | Some improves

--- src/FaigerSYS/superBAR/ConfigUpdate.php
+++ src/FaigerSYS/superBAR/ConfigUpdate.php
@@ -7,26 +7,26 @@ use pocketmine\utils\TextFormat as CLR;
 class ConfigUpdate extends PluginBase {
 
 	public function update($main) {
-		$ver = $main->config->get("ver");
+		$ver = $main->config->get('ver');
 		if ($ver == 1) {
-			$main->getLogger()->info(CLR::RED . "UPDATING CONFIG [ 1->2 ]...");
+			$main->getLogger()->info(CLR::RED . 'UPDATING CONFIG [ 1->4 ]...');
 			$this->u1($main);
-			$main->getLogger()->info(CLR::RED . "UPDATED!!!");
-		}
-		if ($ver == 2) {
-			$main->getLogger()->info(CLR::RED . "UPDATING CONFIG [ 2->3 ]...");
 			$this->u2($main);
-			$main->getLogger()->info(CLR::RED . "UPDATED!!!");
+			$main->getLogger()->info(CLR::RED . 'UPDATED!!!');
+		} elseif ($ver == 2 || $ver == 3) {
+			$main->getLogger()->info(CLR::RED . "UPDATING CONFIG [ $ver->4 ]...");
+			$this->u2($main);
+			$main->getLogger()->info(CLR::RED . 'UPDATED!!!');
 		}
 	}
 	
 	public function u1($main) {
 		$all = $main->config->getAll();
-		$all["format"] = str_replace("%ITEM%", "%ITEM_ID%:%ITEM_META%", $all["format"]);
-		file_put_contents($main->getDataFolder() . "config.yml", $main->getResource("config.yml"));
+		$all['format'] = str_replace('%ITEM%', '%ITEM_ID%:%ITEM_META%', $all['format']);
+		file_put_contents($main->getDataFolder() . 'config.yml', $main->getResource('config.yml'));
 		
-		$conf = file($main->getDataFolder() . "config.yml");
-		$conf[6] = 'hot-format: "' . str_replace("\n", "\\n", $all["format"]) . "\"\n";
+		$conf = file($main->getDataFolder() . 'config.yml');
+		$conf[6] = 'hot-format: "' . str_replace("\n", '\n', $all["format"]) . "\"\n";
 		$conf[38] = 'type: "' . $all["type"] . "\"\n";
 		$conf[44] = 'timer: ' . $all["timer"] . "\n";
 		$conf[51] = 'time-format: "' . $all["time-format"] . "\"\n";
@@ -39,7 +39,7 @@ class ConfigUpdate extends PluginBase {
 		file_put_contents($main->getDataFolder() . "config.yml", $main->getResource("config.yml"));
 		
 		$conf = file($main->getDataFolder() . "config.yml");
-		$conf[6] = 'hot-format: "' . str_replace("\n", "\\n", $all["hot-format"]) . "\"\n";
+		$conf[6] = 'hot-format: "' . str_replace("\n", '\n', $all["hot-format"]) . "\"\n";
 		$conf[31] = 'text-offset-level: ' . $all["text-offset-level"] . "\n";
 		$conf[38] = 'type: "' . $all["type"] . "\"\n";
 		$conf[44] = 'timer: ' . $all["timer"] . "\n";
@@ -47,4 +47,5 @@ class ConfigUpdate extends PluginBase {
 		$conf[60] = 'no-faction: "' . $all["no-faction"] . "\"\n";
 		file_put_contents($main->getDataFolder() . "config.yml", join('', $conf));
 	}
+	
 }
