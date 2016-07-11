21ec72fcef70854199db0a533ae1f9bb1d75a3b0
FaigerSYS <faiger.sys@gmail.com>
2016-07-11 16:42:29 +0300

    Ooops.....

--- resources/addons.php
+++
@@ -1,3 +0,0 @@
-You can create addons for superBAR
-Addon must be in '.php' format. You must create addon from blank, which you can find near this file.
-After you create addon, you must place it at 'addons' folder.
